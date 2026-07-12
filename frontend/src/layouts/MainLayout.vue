5c8acac5869507f50ea8dae98cabe03ca2bce94b
aravindkunnath <10529541+aravindkunnath@users.noreply.github.com>
2026-07-12 14:05:32 -0400

    sidebar updates

--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -2,15 +2,18 @@
 import { SidebarInset } from "@/components/ui/sidebar";
 import "vue-sonner/style.css";
 import { Toaster } from "@/components/ui/sonner";
-import VSCodeSidebar from "@/components/VSCodeSidebar.vue";
+import LeftHandSidebar from "@/components/LeftHandSidebar.vue";
 </script>
 
 <template>
-  <div class="flex h-full w-full overflow-hidden">
-    <VSCodeSidebar />
-    <SidebarInset class="bg-transparent flex flex-col flex-1" style="background-color: transparent !important;">
-      <router-view />
-    </SidebarInset>
-    <Toaster />
-  </div>
+    <div class="flex h-full w-full overflow-hidden">
+        <LeftHandSidebar />
+        <SidebarInset
+            class="bg-transparent flex flex-col flex-1"
+            style="background-color: transparent !important"
+        >
+            <router-view />
+        </SidebarInset>
+        <Toaster />
+    </div>
 </template>
